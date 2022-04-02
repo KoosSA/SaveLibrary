2c1391b3a768b58f3a896358010f6adc3eac9fe0
KoosSA <kkposbus@gmail.com>
2022-04-02 13:39:18 +0200

    Update to include custom folders and names

--- src/main/java/com/koossa/savelib/SaveSystem.java
+++ src/main/java/com/koossa/savelib/SaveSystem.java
@@ -10,27 +10,37 @@ import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
 public class SaveSystem {
-	
+
 	private static Gson gson;
 	private static File saveFolder;
+	private static File dataFolder;
 	private static String writeStr;
 	private static File readFile, writeFile;
-	
-	public static void init(File folder) {
+
+	public static void init(File saveFolder, File dataFolder) {
 		gson = new Gson();
-		saveFolder = folder;
+		SaveSystem.saveFolder = saveFolder;
+		SaveSystem.dataFolder = dataFolder;
 		if (!saveFolder.exists()) {
 			saveFolder.mkdirs();
 		}
+		if (!dataFolder.exists()) {
+			dataFolder.mkdirs();
+		}
 	}
-	
-	public static boolean save(Object obj) {
+
+	public static boolean save(Object obj, boolean isGameData, String subfolder, String name) {
+		File folder = isGameData ? dataFolder : saveFolder;
+		File sub = new File(folder, subfolder);
+		if (!sub.exists()) {
+			sub.mkdirs();
+		}
 		if (gson == null) {
 			System.err.println("Please Initialise SaveSystem, aborting saving file.");
 			return false;
 		}
 		writeStr = gson.toJson(obj);
-		writeFile = new File(saveFolder, obj.getClass().getSimpleName() + ".json");
+		writeFile = new File(sub, name + ".json");
 		if (writeStr.length() > 0) {
 			try {
 				FileWriter writer = new FileWriter(writeFile);
@@ -41,24 +51,29 @@ public class SaveSystem {
 				System.err.println("Failed to save file: " + obj.getClass().getSimpleName() + ".json");
 				e.printStackTrace();
 				return false;
-			} 
+			}
 		}
 		return false;
 	}
-	
-	public static <T> T load(Class<T> clazz) {
+
+	public static <T> T load(Class<T> clazz, boolean isGameData, String subfolder, String name) {
 		if (gson == null) {
 			System.err.println("Please Initialise SaveSystem, aborting file loading.");
 			return null;
 		}
+		File folder = isGameData ? dataFolder : saveFolder;
+		File sub = new File(folder, subfolder);
+		if (!sub.exists()) {
+			sub.mkdirs();
+		}
 		try {
-			readFile = new File(saveFolder, clazz.getSimpleName() + ".json");
+			readFile = new File(sub, name + ".json");
 			JsonReader reader = new JsonReader(new FileReader(readFile));
 			Object o = gson.fromJson(reader, clazz);
 			reader.close();
 			return clazz.cast(o);
 		} catch (IOException e) {
-			System.err.println("Failed to load file: " + clazz.getSimpleName() + ".json. Creating default implementation.");
+			System.err.println("Failed to load file: " + name + ".json. Creating default implementation.");
 			try {
 				T o = clazz.cast(clazz.getConstructors()[0].newInstance());
 				if (o instanceof ISavable) {
@@ -73,5 +88,5 @@ public class SaveSystem {
 		}
 		return null;
 	}
-	
+
 }
